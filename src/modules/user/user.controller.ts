1b8c40b7df7cf64238918d6a557e353606d29dec
KozielGPC <gpcgabriel0@gmail.com>
2022-07-16 17:09:04 -0300

    added createUser

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Prisma } from 'prisma/@generated';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
+
+  @Post()
+  async create(@Body() data: Prisma.UserCreateInput) {
+    return this.userService.create(data);
+  }
+
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from 'prisma/@generated';
+import { PrismaService } from 'src/database/PrismaService';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma: PrismaService) { }
+
+    async create(data: Prisma.UserCreateInput) {
+        return this.prisma.user.create({
+            data: data
+        })
+    }
+}
