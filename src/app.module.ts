ce5f7826060312b65b507ed7e370cc83727bf4e5
KozielGPC <gpcgabriel0@gmail.com>
2022-07-31 20:04:46 -0300

    add: socket connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { UserModule } from './modules/user/user.module';
 import { MatchModule } from './modules/match/match.module';
 import { RoomModule } from './modules/room/room.module';
 import { RoundModule } from './modules/round/round.module';
+import { SocketGateway } from './socket/socket.gateway';
 
 @Module({
-  imports: [UserModule, MatchModule, RoomModule, RoundModule],
-  controllers: [],
-  providers: [],
+    imports: [UserModule, MatchModule, RoomModule, RoundModule],
+    controllers: [],
+    providers: [SocketGateway],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
-  await app.listen(process.env.PORT || 3000);
+    const app = await NestFactory.create(AppModule, { cors: true });
+    const port = process.env.PORT || 3000;
+    await app.listen(port, () => {
+        console.log('Server listening on port ' + port);
+    });
 }
 bootstrap();
